7b671787483424a46b3e4b91c06a7ceef5ba4dda
root <root@debian11.myguest.virtualbox.org>
2023-10-29 17:19:37 -0600

    Creacion del controlador de las comidas y funciones para mostrar las categorias

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :meals do
+    collection do
+      get 'categories'
+      get 'recipes/:category', action: 'recipes_by_category', as: 'recipes_by_category'
+    end
+  end
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
