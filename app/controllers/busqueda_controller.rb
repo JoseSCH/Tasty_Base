9ec9b4baa99e60e35503d27118c7b35748219b2b
jhovanny <jhovannyortiz015@gmail.com>
2023-10-30 23:17:33 -0600

    implementacion de cache para consumo mas rapido

--- app/controllers/busqueda_controller.rb
+++ app/controllers/busqueda_controller.rb
@@ -1,14 +1,18 @@
 class BusquedaController < ApplicationController
-    require 'net/http' # Asegúrate de requerir la biblioteca Net::HTTP
-    require 'json' 
-
+    require 'net/http'
+    require 'json'
+  
     def buscar
-        @query = params[:query]
-        query = CGI.escape(@query)  # Utiliza CGI.escape para escapar la consulta
+      @query = params[:query]
+      query = CGI.escape(@query)
+      
+      # Intenta obtener los resultados de la caché
+      @resultados = Rails.cache.fetch("search_results_#{query}", expires_in: 1.hour) do
         api_url = "https://www.themealdb.com/api/json/v1/1/search.php?s=#{query}"
         response = Net::HTTP.get(URI(api_url))
         data = JSON.parse(response)
-        @resultados = data['meals'] || []
+        data['meals'] || []  # Estos son los resultados que se almacenarán en caché
+      end
     end
   end
   
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,8 +3,10 @@ class MealsController < ApplicationController
   require 'json'
 
   def categories
+    # Usa la caché para almacenar/recuperar los resultados
+    @recipes_by_category = Rails.cache.fetch('recipes_by_category', expires_in: 1.hour) do
       categories = ["Beef", "Breakfast", "Chicken", "Dessert", "Goat", "Lamb", "Miscellaneous", "Pasta", "Pork", "Seafood", "Side", "Starter", "Vegan", "Vegetarian"]
-      @recipes_by_category = {}
+      result = {}
 
       categories.each do |category|
         # Construye la URL de la API para obtener recetas por categoría
@@ -17,19 +19,25 @@ class MealsController < ApplicationController
         recipes = JSON.parse(response)['meals']
 
         # Limita la cantidad de recetas a un máximo de 4
-        @recipes_by_category[category] = recipes.take(4)
+        result[category] = recipes.take(4)
       end
+
+      result
     end
+  end
 
-    def recipes_by_category
-        category = params[:category]
-        # Construye la URL de la API para obtener recetas por categoría
-        api_url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{category}"
+  def recipes_by_category
+    category = params[:category]
+    # Usa la caché para almacenar/recuperar los resultados
+    @recipes = Rails.cache.fetch("recipes_by_category_#{category}", expires_in: 1.hour) do
+      # Construye la URL de la API para obtener recetas por categoría
+      api_url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{category}"
 
-        # Realiza la solicitud a la API
-        response = Net::HTTP.get(URI(api_url))
+      # Realiza la solicitud a la API
+      response = Net::HTTP.get(URI(api_url))
 
-        # Convierte la respuesta JSON en un objeto Ruby
-        @recipes = JSON.parse(response)['meals']
-      end
+      # Convierte la respuesta JSON en un objeto Ruby
+      JSON.parse(response)['meals']
+    end
+  end
 end
