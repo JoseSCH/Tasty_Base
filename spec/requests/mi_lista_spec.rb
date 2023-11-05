cd9e3ac1c66f8384d85f7eb5a0e7cbaad4687910
cheppeto <jsozachevez@gmail.com>
2023-11-05 13:08:49 -0600

    Mejoras de seguridad

--- spec/requests/mi_lista_spec.rb
+++ spec/requests/mi_lista_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe "MiLista", type: :request do
   include Devise::Test::IntegrationHelpers
 
+  correo = Rails.application.config.email_for_testing
+  contra = Rails.application.config.password_for_testing
+
   it "Verificar el acceso a la pagina de lista del usuario" do
-    usuario = User.create(email: "rodrick@lol.ni", password: "12345678")
+    usuario = User.create(email: correo, password: contra)
     sign_in usuario
     get mi_lista_path
     expect(response).to have_http_status(:success)
--- spec/requests/pdfnizator_spec.rb
+++ spec/requests/pdfnizator_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe "Pdfnizators", type: :request do
   include Devise::Test::IntegrationHelpers
 
+  correo = Rails.application.config.email_for_testing
+  contra = Rails.application.config.password_for_testing
+
   it "Verificar el acceso al pdf" do
-    usuario = User.create(email: "rodrick@lol.ni", password: "12345678")
+    usuario = User.create(email: correo, password: contra)
     sign_in usuario
     stub_request(:any, /.*/).to_return(status: 406)
     get pdfizador_path
