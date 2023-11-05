4d787d58f42c2cb6f6f23482ad997bcb119d5b07
cheppeto <jsozachevez@gmail.com>
2023-11-05 13:04:02 -0600

    Mejoras de seguridad

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   include Devise::Test::IntegrationHelpers
 
+  correo = Rails.application.config.email_for_testing
+  contra = Rails.application.config.password_for_testing
+  contra_fake = Rails.application.config.password_for_testing_fake
+
   it "usuario con atributos validos" do
-    expect(User.new(email: "rodrick@lol.ni", password: "12345678")).to be_valid
+    expect(User.new(email: correo, password: contra)).to be_valid
   end
 
   it "usuario con atributos no validos" do
@@ -12,12 +16,12 @@ RSpec.describe User, type: :model do
   end
 
   it "No pueden haber usuarios duplicados" do
-    User.create(email: "rodrick@lol.ni", password: "12345678")
-    expect(User.new(email: "rodrick@lol.ni", password: "87654321")).to be_invalid
+    User.create(email: correo, password: contra)
+    expect(User.new(email: correo, password: contra_fake)).to be_invalid
   end
 
   it "Puede el usuario eliminar la cuenta?" do
-    usuario = User.create(email: "rodrick@lol.ni", password: "12345678")
+    usuario = User.create(email: correo, password: contra)
     usuario.delete
     expect(usuario).to be_destroyed
   end
